932398c3d90338fed4641a26680520caf1a34447
ahmed nader <ahmednader5012@gmail.com>
2025-07-26 14:34:40 +0300

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
 import { Alex_Brush, Mada } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -48,6 +49,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body className={`${mada.variable} ${alexBrush.variable}`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
